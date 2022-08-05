e4a473ff2f908c1ff3fba3317584eb85c366ae31
NouemanKHAL <noueman.khal@gmail.com>
2022-08-05 22:08:47 +0200

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -6,15 +6,20 @@ import (
 	"os"
 )
 
+func toRestSimpleString(s string) string {
+	return fmt.Sprintf("+%s\r\n", s)
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn.Write([]byte(toRestSimpleString("PONG")))
 }
